f1ec5c2b7fd9d0aa54afcfcabb6b1eff39d8d7a6
mixsmvrt-ai <mixsmvrt@gmail.com>
2026-08-04 19:10:17 -0500

    Delete routes directory

--- routes/music.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as controller from "../controllers/musicInterpretation.controller.js";
-import { requireAuth } from "../middleware/auth.js";
-
-export const musicRouter = Router();
-musicRouter.use(requireAuth);
-musicRouter.post("/interpret", controller.interpret);
-musicRouter.get("/interpret/:id", controller.read);
-musicRouter.delete("/interpret/:id", controller.remove);
